269086e2b721f6d7f088e0f0e57fb8ecd5a9940c
star_fx <star.fx27@gmail.com>
2019-01-12 22:20:35 +0800

    add arg parse support to whodeps

--- whodeps.py
+++ whodeps.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3.7
 
+import argparse
 import json
 import shlex
 import subprocess
-import sys
+
+
+def arg_parse():
+    parser = argparse.ArgumentParser(description="do the revert of brew deps.")
+    parser.add_argument("formula_name")
+    args = parser.parse_args()
+    return args
 
 
 def main():
+    args = arg_parse()
     brew_cmd = "brew info --installed --json"
     result = subprocess.run(shlex.split(brew_cmd), capture_output=True)
     for formula in json.loads(result.stdout):
-        if sys.argv[1] in formula["dependencies"]:
+        if args.formula_name in formula["dependencies"]:
             print(formula["name"])
  
 
